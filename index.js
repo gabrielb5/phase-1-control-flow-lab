0037730ec8112ee1552f75c5ef2bce4a4935e8e1
Gabriel Burgos <gabrielburgosmm@gmail.com>
2022-06-27 16:36:26 -0700

    commit tree db1ded889ecaecba132a3d1da5b51d295d128d7d

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet (ride) {
+  if (ride <= 400) {
+    return 'This one is on me!';
+  } else if (ride <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (2000 < ride && ride <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? "Ok, sounds good." : "No go." }
+
+
+
+function switchOnCharmFromTip(tip){
+  if (tip === "generous") {
+    return "Thank you so much.";
+  } else if (tip === "not as generous") {
+    return "Thank you.";
+  } else if (tip)
+    return "Bye.";
 }
